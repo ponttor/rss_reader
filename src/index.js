fe8aab69bec0642cacd771b0c9edde05e50470c5
ponttor <alex@heimstudio.ru>
2021-02-27 19:11:57 +0100

    showing rss first time

--- src/index.js
+++ src/index.js
@@ -7,11 +7,6 @@ import axios from 'axios';
 import parse from './parserRss.js';
 import resources from './locales/index.js';
 import '../_custom.scss';
-// import { render } from 'node-sass';
-
-// import request from './request.js';
-
-// const { addDevServerEntrypoints } = require("webpack-dev-server");
 
 i18next.init({
   lng: 'ru',
@@ -38,7 +33,6 @@ const state = {
   feeds: [],
   form: {
     processState: 'filling',
-    // processError: null,
     fields: {
       url: '',
     },
@@ -55,26 +49,17 @@ const render = ({ feeds }) => {
   result.innerHTML = htmlTitle + htmlDescription + htmlItemsList;
 };
 
-// form, loadingProcess, feed
 const watchedState = onChange(state, (path, value) => {
-  // alert('value changed!');
   switch (path) {
     case 'loadingProcess.status':
-      if ((value === 'sending') && (state.form.valid)) { // показать лоадер
+      if ((value === 'sending') && (state.form.valid)) {
         button.setAttribute('disabled', true);
 
         axios.get(state.form.fields.url)
           .then((response) => {
             const newFeed = parse(response.data);
-            console.log(newFeed);
             watchedState.feeds.unshift(newFeed);
-            console.log(state.feeds);
             watchedState.loadingProcess.status = 'idle';
-            // const { title, description, items } = parse(response.data);
-            // const newRss = { title, description, posts };
-            // state.rss.push(newRss);
-            // console.log(`title: ${newFeed.title}, description: ${newFeed.description}`);
-            // console.log(`items: ${newFeed.items}`);
           })
           .catch((error) => {
             watchedState.loadingProcess.error = error.message; // Net error vs no Rss error
@@ -94,7 +79,6 @@ const watchedState = onChange(state, (path, value) => {
           el.classList.add('feedback');
           el.textContent = i18next.t('validationError');
           inner.append(el);
-          alert('error');
         } else {
           (document.querySelector('.feedback')).textContent = i18next.t('validationError');
         }
@@ -131,7 +115,6 @@ const watchedState = onChange(state, (path, value) => {
         el.classList.add('feedback');
         el.textContent = i18next.t('NetError');
         inner.append(el);
-        alert('error');
       } else {
         (document.querySelector('.feedback')).textContent = i18next.t('NetError');
       }
@@ -164,93 +147,12 @@ const handleInput = (e) => {
   watchedState.form.processState = 'filling';
   watchedState.form.fields.url = e.target.value;
   updateValidationState(watchedState);
-  // if (state.form.valid) {
-  //   input.classList.remove('is-invalid');
-  // }
 };
 
 const handleSubmit = (e) => {
   e.preventDefault();
   watchedState.loadingProcess.status = 'sending';
-  // console.log(state.form.valid);
-  // if (watchedState.form.valid === false) {
-  //   // input.classList.add('is-invalid', true);
-  //   return;
-  // }
-
-  // watchedState.form.processState = 'sending';
-
-  // try {
-  //   await axios.post(routes.usersPath(), watchedState.form.fields);
-  //   watchedState.form.processState = 'finished';
-  // } catch (err) {
-  //   // В реальных приложениях также требуется корректно обрабатывать сетевые ошибки
-  //   watchedState.form.processError = errorMessages.network.error;
-  //   watchedState.form.processState = 'failed';
-  //   // здесь это опущено в целях упрощения приложения
-  //   throw err;
-  // }
 };
 
 input.addEventListener('input', handleInput);
 form.addEventListener('submit', handleSubmit);
-
-// const watchedState = onChange(state, (path, value) => {
-//   if (path === 'state.form.processState') {
-//     if (value === 'sending') {
-//       button.setAttribute('disabled');
-//       input.value = '';
-//     }
-//   }
-// });
-
-// input.addEventListener('change', (e) => {
-//   watchedState.registrationForm.data.name = e.target.value;
-//   // Действия: валидация, запросы, ...
-// });
-
-// form.elements.email.addEventListener('change', (e) => {
-//   watchedState.registrationForm.data.email = e.target.value;
-//   // Действия: валидация, запросы, ...
-// });
-
-// button.addEventListener('onclick', handleClick);
-
-// };
-// const handleSubmit = (e) => {
-//   e.preventDefault();
-//   schema
-//     .isValid({
-//       input: state.input,
-//     })
-//     .then((content) => {
-//       if ((!content) || (_.includes(state.content, state.input))) {
-//         // alert(`${state.content}, ${content}, ${state.input}`);
-//         // input.reset();
-//         input.classList.add('is-invalid');
-//       } else {
-//         state.content.unshift(state.input);
-//         const result = state.content.join('\n');
-//         alert(result);
-//         form.reset();
-//         // return false;
-//       }
-//     });
-// };
-
-// const handleClick = (e) => {
-//   e.preventDefault();
-// };
-// const isValid = await schema.isValid(inputContent);
-// alert(isValid);
-
-//   alert(validation(state.input));
-// if (validation(state)) {
-//   alert('valid');
-//   state.content.unshift(state.input);
-//   console.log(state.content);
-
-// input.removeClass('action');
-// input.classList.add('is-invalid');
-// alert('ok');
-// };
